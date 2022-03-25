7cfe803683054a1baedabba0795e501504657548
Hazem-smawy <96139383+Hazem-smawy@users.noreply.github.com>
2022-03-25 18:34:21 +0300

    custom domains

--- Config/index.js
+++ Config/index.js
@@ -1,2 +1,2 @@
 const dev = process.env.NODE_ENV !== 'production'
-export const  server = dev ? 'http://localhost:5000':'http://github.com/Hazem-smawy/logyn'
+export const  server = dev ? 'http://localhost:5000':'http://github.com/Hazem-smawy/netLogyin'
