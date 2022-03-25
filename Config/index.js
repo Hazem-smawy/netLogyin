5c62889fcc3e4ce72298bf693eb9486c51c23c61
Hazem-smawy <96139383+Hazem-smawy@users.noreply.github.com>
2022-03-25 19:00:17 +0300

    abdate

--- Config/index.js
+++ Config/index.js
@@ -1,2 +1,2 @@
 const dev = process.env.NODE_ENV !== 'production'
-export const  server = dev ? 'http://localhost:5000':'http://github.com/Hazem-smawy/netLogyin'
+export const  server = dev ? 'http://localhost:5000':'https://calm-chaja-9bca66.netlify.app/'
