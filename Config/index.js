1cfc5c8a57ba13780177d92bc8f6a15523dfbac6
Hazem-smawy <96139383+Hazem-smawy@users.noreply.github.com>
2022-03-25 19:08:18 +0300

    Update index.js

--- Config/index.js
+++ Config/index.js
@@ -1,2 +1,2 @@
 const dev = process.env.NODE_ENV !== 'production'
-export const  server = dev ? 'http://localhost:5000':'https://calm-chaja-9bca66.netlify.app/'
+export const  server = dev ? 'http://localhost:5000':'logyn'
